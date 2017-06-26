dfa6ca7337e4cb226305f8d8965579115b32a3ee
SimonG96 <simon.gockner@gmail.com>
2017-06-26 23:15:05 +0200

    - add new class LogicPuzzle
    - main-method
    - static Array of Levels -> List of all Levels available

---
+++ src/LogicPuzzle.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Acer on 26.06.2017.
+ */
+public class LogicPuzzle {
+    public static final Level[] LEVELS = null;
+
+    public LogicPuzzle()
+    {
+        //TODO: LEVELS = ...
+    }
+
+    public static void main(String args[])
+    {
+
+    }
+}
