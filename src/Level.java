a77cd5563838e77a284bf41e877687dfa788c2de
SimonG96 <simon.gockner@gmail.com>
2017-06-26 23:13:10 +0200

    - add Properties and Getters

--- src/Level.java
+++ src/Level.java
@@ -1,6 +1,46 @@
+import Ball.Ball;
+import Tile.Tile;
+
 /**
  * Created by s.gockner on 19.06.2017.
  */
 public class Level {
+    public Level(int levelNumber, Tile[][] tiles, Ball[] balls, String tipp)
+    {
+        LevelNumber = levelNumber;
+        Tiles = tiles;
+        Balls = balls;
+        Tipp = tipp;
+    }
 
+    private int LevelNumber;
+    private Tile[][] Tiles;
+    private Ball[] Balls;
+    private String Tipp;
+
+
+    public int GetLevelNumber()
+    {
+        return LevelNumber;
+    }
+
+    public Tile[][] GetTiles()
+    {
+        return Tiles;
+    }
+
+    public Ball[] GetBalls()
+    {
+        return Balls;
+    }
+
+    public int GetNumberOfBalls()
+    {
+        return Balls.length;
+    }
+
+    public String GetTipp()
+    {
+        return Tipp;
+    }
 }
