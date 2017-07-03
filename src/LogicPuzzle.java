6768f6780664e6237d1d04315ba9c82937fdd597
SimonG96 <simon.gockner@gmail.com>
2017-07-03 23:21:26 +0200

    - main method creates a new Game Panel

--- src/LogicPuzzle.java
+++ src/LogicPuzzle.java
@@ -1,16 +1,15 @@
+import Ball.*;
+
 /**
  * Created by Acer on 26.06.2017.
  */
 public class LogicPuzzle {
-    public static final Level[] LEVELS = null;
-
-    public LogicPuzzle()
-    {
-        //TODO: LEVELS = ...
-    }
 
     public static void main(String args[])
     {
-
+        GamePanel _gamePanel = new GamePanel(800, 800);
     }
+
+    //TODO: Access to Balls always and only over ID! Don't use index in Array/List because this might possibly change. The ID always stays the same
+
 }
